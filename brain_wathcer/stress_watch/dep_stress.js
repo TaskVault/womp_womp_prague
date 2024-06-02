7587e768e58f14437f0f703574af48fdbcd82566
yzheng <posterscofield@gmail.com>
2024-06-02 10:12:52 +0200

    modified stress deployment

--- brain_wathcer/stress_watch/dep_stress.js
+++ brain_wathcer/stress_watch/dep_stress.js
@@ -1,10 +1,13 @@
+const { ethers } = require("hardhat");
+
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
   const StressLevel = await ethers.getContractFactory("StressLevel");
   const stressLevel = await StressLevel.deploy();
 
-  await stressLevel.deployed();
-
-  console.log("StressLevel contract deployed to:", stressLevel.address);
+  console.log("StressLevel contract address:", stressLevel.address);
 }
 
 main()
